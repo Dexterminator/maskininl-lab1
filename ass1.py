0faa76d56fbd19fce51bcfea0442f4fd7c2d21c1
Andreas Johansson <andreas.cm.johansson@gmail.com>
2015-09-07 19:57:15 +0200

    added first assignment

---
+++ ass1.py
@@ -0,0 +1,7 @@
+import monkdata as m
+import dtree as dt
+
+print "Entropy\n"
+print "Monk1: " + str(dt.entropy(m.monk1))
+print "Monk2: " +  str(dt.entropy(m.monk2))
+print "Monk3: " +  str(dt.entropy(m.monk3))
