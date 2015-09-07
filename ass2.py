ef31c2d1a12d27a8aabb500d07159ed86b0c9185
Andreas Johansson <andreas.cm.johansson@gmail.com>
2015-09-07 20:28:07 +0200

    added second assignment

---
+++ ass2.py
@@ -0,0 +1,16 @@
+import monkdata as m
+import dtree as dt
+
+def printAverageGain(s, dataset):
+	for x in xrange(0,6):
+		s = s + str(dt.averageGain(dataset, m.attributes[x])) + " "
+		pass
+	print s
+	pass
+
+
+	print "Average gain\n"
+
+printAverageGain("Monk1: ", m.monk1)
+printAverageGain("Monk2: ", m.monk2)
+printAverageGain("Monk3: ", m.monk3)
