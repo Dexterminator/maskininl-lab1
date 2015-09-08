f2470da2c62dc6d07d6bd6f515aa19c4d03a02f5
Dexter Gramfors <dexter.gramfors@gmail.com>
2015-09-08 13:21:45 +0200

    Print classification errors for all datasets and fractions.

--- ass4.py
+++ ass4.py
@@ -2,6 +2,30 @@ import monkdata as m
 import dtree as dt
 import random
 
+FRACTIONS = [0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+AGGREGATE_TIMES = 1000
+
+
+def main():
+    datasets = [('monk1', m.monk1, m.monk1test), ('monk2', m.monk2, m.monk2test), ('monk3', m.monk3, m.monk3test)]
+    for name, training_set, test_set in datasets:
+        print(name)
+        print_non_pruned_performance(test_set, training_set)
+        print_pruned_performances(training_set)
+        print()
+
+
+def print_non_pruned_performance(test_set, training_set):
+    non_pruned_tree = dt.buildTree(training_set, m.attributes)
+    performance_without_pruning = dt.check(non_pruned_tree, test_set)
+    print('Performance without pruning: {}'.format(performance_without_pruning))
+
+
+def print_pruned_performances(training_set):
+    for fraction in FRACTIONS:
+        average_performance = pruned_tree_performance(training_set, fraction)
+        print('Pruning performance with fraction {}: {}'.format(fraction, average_performance))
+
 
 def partition(data, fraction):
     ldata = list(data)
@@ -10,17 +34,8 @@ def partition(data, fraction):
     return ldata[:breakPoint], ldata[breakPoint:]
 
 
-def main():
-    dataset = m.monk1
-    fractions = [0.3, 0.4, 0.5, 0.6, 0.7, 0.8]
-    for fraction in fractions:
-        average_performance = pruned_tree_performance(dataset, fraction)
-        print(fraction, average_performance)
-
-
 def pruned_tree_performance(dataset, fraction):
     total_performance = 0
-    AGGREGATE_TIMES = 1000
     for i in range(AGGREGATE_TIMES):
         tree = best_pruned_tree(dataset, fraction)
         total_performance += dt.check(tree, m.monk1test)
@@ -29,14 +44,14 @@ def pruned_tree_performance(dataset, fraction):
 
 
 def best_pruned_tree(dataset, fraction):
-    monk1train, monk1val = partition(dataset, fraction)
-    tree = dt.buildTree(monk1train, m.attributes)
+    train, val = partition(dataset, fraction)
+    tree = dt.buildTree(train, m.attributes)
     improved = True
     while improved:
         improved = False
-        best_performance = dt.check(tree, monk1val)
+        best_performance = dt.check(tree, val)
         for pruned_tree in dt.allPruned(tree):
-            performance = dt.check(pruned_tree, monk1val)
+            performance = dt.check(pruned_tree, val)
             if performance > best_performance:
                 best_performance = performance
                 tree = pruned_tree
