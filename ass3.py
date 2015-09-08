e42e08fe098372df1c2b037222b8fc33699613a1
Dexter Gramfors <dexter.gramfors@gmail.com>
2015-09-08 10:33:31 +0200

    ass3 done.

--- ass3.py
+++ ass3.py
@@ -1,1 +1,20 @@
-__author__ = 'dexter'
+import monkdata as m
+import dtree as d
+
+t = d.buildTree(m.monk1, m.attributes)
+print('monk1')
+print(d.check(t, m.monk1test))
+print(d.check(t, m.monk1))
+print()
+
+print('monk2')
+t = d.buildTree(m.monk2, m.attributes)
+print(d.check(t, m.monk2test))
+print(d.check(t, m.monk2))
+print()
+
+print('monk3')
+t = d.buildTree(m.monk3, m.attributes)
+print(d.check(t, m.monk3test))
+print(d.check(t, m.monk3))
+
