0c027396d67ec99ab51a2b24b13689bcdb42cc0f
Dexter Gramfors <dexter.gramfors@gmail.com>
2015-09-08 13:44:55 +0200

    Test using proper test data and print best fraction for each dataset.

--- ass4.py
+++ ass4.py
@@ -10,35 +10,33 @@ def main():
     datasets = [('monk1', m.monk1, m.monk1test), ('monk2', m.monk2, m.monk2test), ('monk3', m.monk3, m.monk3test)]
     for name, training_set, test_set in datasets:
         print(name)
-        print_non_pruned_performance(test_set, training_set)
-        print_pruned_performances(training_set)
+        print_non_pruned_performance(training_set, test_set)
+        print_pruned_performances(training_set, test_set)
         print()
 
 
-def print_non_pruned_performance(test_set, training_set):
+def print_non_pruned_performance(training_set, test_set):
     non_pruned_tree = dt.buildTree(training_set, m.attributes)
     performance_without_pruning = dt.check(non_pruned_tree, test_set)
     print('Performance without pruning: {}'.format(performance_without_pruning))
 
 
-def print_pruned_performances(training_set):
+def print_pruned_performances(training_set, test_set):
+    best_performance = 0
     for fraction in FRACTIONS:
-        average_performance = pruned_tree_performance(training_set, fraction)
+        average_performance = pruned_tree_performance(training_set, test_set, fraction)
+        if average_performance > best_performance:
+            best_performance = average_performance
+            best_fraction = fraction
         print('Pruning performance with fraction {}: {}'.format(fraction, average_performance))
+    print('Best fraction: {}: {}'.format(best_fraction, best_performance))
 
 
-def partition(data, fraction):
-    ldata = list(data)
-    random.shuffle(ldata)
-    breakPoint = int(len(ldata) * fraction)
-    return ldata[:breakPoint], ldata[breakPoint:]
-
-
-def pruned_tree_performance(dataset, fraction):
+def pruned_tree_performance(training_set, test_set, fraction):
     total_performance = 0
     for i in range(AGGREGATE_TIMES):
-        tree = best_pruned_tree(dataset, fraction)
-        total_performance += dt.check(tree, m.monk1test)
+        tree = best_pruned_tree(training_set, fraction)
+        total_performance += dt.check(tree, test_set)
     average_performance = total_performance / AGGREGATE_TIMES
     return average_performance
 
@@ -57,6 +55,13 @@ def best_pruned_tree(dataset, fraction):
                 tree = pruned_tree
                 improved = True
     return tree
+
+
+def partition(data, fraction):
+    ldata = list(data)
+    random.shuffle(ldata)
+    breakPoint = int(len(ldata) * fraction)
+    return ldata[:breakPoint], ldata[breakPoint:]
 
 
 if __name__ == '__main__':
